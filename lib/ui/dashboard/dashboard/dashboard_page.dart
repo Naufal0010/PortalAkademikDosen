387a450698842519dc76c72652977f6b4cc892f8
Naufal Abiyyu <naufal1068@gmail.com>
2022-06-13 14:36:39 +0800

    Customize the drawer_navigator.dart

--- lib/ui/dashboard/dashboard/dashboard_page.dart
+++ lib/ui/dashboard/dashboard/dashboard_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:portal_akademik_dosen/utils/color_pallete.dart';
+import 'package:portal_akademik_dosen/ui/dashboard/drawer/drawer_navigator.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -10,64 +9,16 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  double xOffset = 0;
-  double yOffset = 0;
-  double scaleFactor = 1;
-  bool isDrawer = false;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        setState(() {
-          xOffset = 0;
-          yOffset = 0;
-          scaleFactor = 1;
-          isDrawer = false;
-          SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-              statusBarColor: Colors.grey
-          ));
-        });
-      },
-      child: AnimatedContainer(
-        transform: Matrix4.translationValues(xOffset, yOffset, 0)
-          ..scale(scaleFactor),
-        duration: Duration(milliseconds: 250),
-        color: Colors.white,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                isDrawer
-                    ? IconButton(
-                        icon: Icon(Icons.arrow_back),
-                        onPressed: () {
-                          setState(() {
-                            xOffset = 0;
-                            yOffset = 0;
-                            scaleFactor = 1;
-                            isDrawer  = false;
-                            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-                                statusBarColor: Colors.grey
-                            ));
-                          });
-                        })
-                    : IconButton(
-                        icon: const Icon(Icons.menu),
-                        onPressed: () {
-                          setState(() {
-                            xOffset = 200;
-                            yOffset = 140;
-                            scaleFactor = 0.6;
-                            isDrawer  = true;
-                            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-                              statusBarColor: ColorPallete.primary
-                            ));
-                          });
-                        }),
-              ],
-            )
-          ],
+    return Scaffold(
+      drawer: const DrawerNavigator(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(
+          color: Colors.black
         ),
       ),
     );
--- lib/ui/dashboard/drawer/drawer_navigator.dart
+++ lib/ui/dashboard/drawer/drawer_navigator.dart
@@ -11,8 +11,204 @@ class DrawerNavigator extends StatefulWidget {
 class _DrawerNavigatorState extends State<DrawerNavigator> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: ColorPallete.primary,
+    return Drawer(
+      child: Material(
+        color: ColorPallete.primary,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 24,
+                    backgroundColor: Colors.grey,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        'Rudy Herteno',
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                      Text(
+                        '19880925201703110001',
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Menu Akademik',
+              style: TextStyle(color: Colors.white60, fontSize: 16),
+            ),
+            buildMenuItem(
+                text: 'Beranda',
+                icon: Icons.home,
+                onClicked: () => selectedItem(context, 0)),
+            buildMenuItem(
+                text: 'Perwalian',
+                icon: Icons.school,
+                onClicked: () => selectedItem(context, 1)),
+            ExpansionTile(
+              leading: const Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'Penilaian',
+                style: TextStyle(color: Colors.white),
+              ),
+              collapsedIconColor: Colors.white,
+              children: [
+                buildMenuItem(
+                    text: 'Semester Berjalan',
+                    icon: Icons.circle,
+                    onClicked: () => selectedItem(context, 2)),
+                buildMenuItem(
+                    text: 'Riwayat',
+                    icon: Icons.circle,
+                    onClicked: () => selectedItem(context, 3)),
+              ],
+            ),
+            buildMenuItem(
+                text: 'Presensi',
+                icon: Icons.people,
+                onClicked: () => selectedItem(context, 4)),
+            const SizedBox(height: 24),
+            const Text(
+              'Menu Perkuliahan',
+              style: TextStyle(color: Colors.white60, fontSize: 16),
+            ),
+            buildMenuItem(
+                text: 'Penawaran Mata Kuliah',
+                icon: Icons.list,
+                onClicked: () => selectedItem(context, 5)),
+            ExpansionTile(
+              leading: const Icon(
+                Icons.calendar_today,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'Jadwal',
+                style: TextStyle(color: Colors.white),
+              ),
+              collapsedIconColor: Colors.white,
+              children: [
+                buildMenuItem(
+                    text: 'Kalender Akademik',
+                    icon: Icons.circle,
+                    onClicked: () => selectedItem(context, 6)),
+                buildMenuItem(
+                    text: 'Jadwal Penting',
+                    icon: Icons.circle,
+                    onClicked: () => selectedItem(context, 7)),
+                buildMenuItem(
+                    text: 'Perkuliahan',
+                    icon: Icons.circle,
+                    onClicked: () => selectedItem(context, 8)),
+                buildMenuItem(
+                    text: 'Ujian Akhir Semester',
+                    icon: Icons.circle,
+                    onClicked: () => selectedItem(context, 9)),
+              ],
+            ),
+            ExpansionTile(
+              leading: const Icon(
+                Icons.question_mark,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'Kuesioner',
+                style: TextStyle(color: Colors.white),
+              ),
+              collapsedIconColor: Colors.white,
+              children: [
+                buildMenuItem(
+                    text: 'Kuesioner Kepuasan',
+                    icon: Icons.circle,
+                    onClicked: () => selectedItem(context, 10)),
+                buildMenuItem(
+                    text: 'Hasil Evaluasi Dosen',
+                    icon: Icons.circle,
+                    onClicked: () => selectedItem(context, 11)),
+              ],
+            ),
+            const Divider(
+              color: Colors.white70,
+            ),
+            buildMenuItem(
+                text: 'Log Out',
+                icon: Icons.logout,
+                onClicked: () => selectedItem(context, 12))
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget buildMenuItem({
+    required String text,
+    required IconData icon,
+    VoidCallback? onClicked,
+  }) {
+    const color = Colors.white;
+    const hoverColor = Colors.white70;
+
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: color,
+      ),
+      title: Text(
+        text,
+        style: const TextStyle(color: color),
+        overflow: TextOverflow.ellipsis,
+      ),
+      hoverColor: hoverColor,
+      onTap: onClicked,
+    );
+  }
+
+  void selectedItem(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+      case 10:
+        break;
+      case 11:
+        break;
+      case 12:
+        break;
+    }
   }
 }
