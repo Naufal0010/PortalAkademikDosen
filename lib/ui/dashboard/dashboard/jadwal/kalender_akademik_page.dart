9ecfe2b5d91024c07f34cb31dc5eaf4731643930
Nanda <ananda.noorrizki@ulm.ac.id>
2022-06-14 00:56:34 +0800

    navbar

---
+++ lib/ui/dashboard/dashboard/jadwal/kalender_akademik_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:portal_akademik_dosen/ui/dashboard/drawer/drawer_navigator.dart';
+
+class KalenderAkademikPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: const Text('Kalender Akademik',
+            style: TextStyle(
+                color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+            overflow: TextOverflow.ellipsis),
+      ),
+    );
+  }
+}
---
+++ lib/ui/dashboard/dashboard/penawaran/penawaranMK_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:portal_akademik_dosen/ui/dashboard/drawer/drawer_navigator.dart';
+
+class PenawaranMKPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: const Text('Data Penawaran Mata Kuliah',
+            style: TextStyle(
+                color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+            overflow: TextOverflow.ellipsis),
+      ),
+    );
+  }
+}
